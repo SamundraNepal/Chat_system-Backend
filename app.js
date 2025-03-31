e05d707cc35980d1c6a6b7f0f178df822a857fdd
Samundra Nepal <samundranepal9818@gmail.com>
2025-03-31 11:51:33 +1100

    final commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //project dependency
 const express = require("express");
 const app = express();
+const cros = require("cors");
 
 //inheritnace
 const userRoutes = require("./Routes/userRoutes");
 
+app.use(cros({ origin: "http://localhost:3001", credentials: true }));
 app.use(express.json()); // Middleware to parse incoming JSON bodies
 
 //allows access to this path files
